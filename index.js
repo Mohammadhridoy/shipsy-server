67d863ab50c67d0346e3fe9221897c590bcb36b4
Mohammadhridoy <mdridoykha019@gmail.com>
2023-11-28 14:59:06 +0600

    get data from mongodb

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
@@ -30,6 +30,7 @@ async function run() {
     await client.connect();
 
     const usrtypeCollection = client.db('shipsy').collection('user');
+    const bookingsCollection = client.db('shipsy').collection('bookings')
 
     // store user info in database
     app.post('/user', async(req, res)=>{
@@ -43,7 +44,102 @@ async function run() {
         const result = await usrtypeCollection.insertOne(user)
         res.send(result)
     })
+    // get user data 
+    app.get('/user', async(req, res)=>{
+        const email = req.query.email
+        
+        const query = {role:"user", email:email}
+        const result = await usrtypeCollection.find(query).toArray()
+        res.send(result)
+    })
 
+    // update user info 
+    app.put('/user/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const options = { upsert: true }
+        const updateduser = req.body; 
+        const updateInfo = {
+          $set:{
+           image: updateduser.image
+          },
+        };
+        const result = await usrtypeCollection.updateOne(filter, updateInfo, options)
+        res.send(result)
+    })
+
+    app.get('/users', async(req, res) =>{
+
+        
+        const query = { role:'user' }
+        
+        const result = await usrtypeCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    // parcels post in database 
+    app.post('/bookings', async(req, res)=>{
+        const bookingInfo= req.body;
+
+        const result = await bookingsCollection.insertOne(bookingInfo)
+        res.send(result)
+    })
+    // get booking info 
+    app.get('/bookings', async(req, res)=>{
+
+        let query = { };
+      if(req.query?.email) {
+        query = {userEmail:req.query.email}
+
+      }
+
+        const result = await bookingsCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    app.get("/bookings/:id", async(req, res ) =>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+  
+        const result = await bookingsCollection.findOne(query)
+        res.send(result)
+  
+      })
+
+    app.put('/bookings/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const options = { upsert: true }
+        const updatedbooking = req.body; 
+        const updateInfo = {
+          $set:{
+            userNumber: updatedbooking.number,
+            parcelType: updatedbooking.parceltype,
+            receiverName: updatedbooking.receivername,
+            receiverNumber: updatedbooking.receiversnumber,
+            deliveryAddress: updatedbooking.address,
+            latitude: updatedbooking.latitude,
+            longitude: updatedbooking.logitude,
+            requestedDeliveryDate: updatedbooking.requestedDeliveryDate,
+          },
+        };
+        const result = await bookingsCollection.updateOne(filter, updateInfo, options)
+        res.send(result)
+      })
+    //   total bookigns fillter by email
+    app.get('/bookings', async(req, res)=>{
+        const email = req.query.email
+        console.log(email)
+        const query = {userEmail:email}
+        const totalBookings = await bookingsCollection.countDocuments(query)
+        res.send(totalBookings)
+
+    })
+
+    app.get('/bookings', async(req, res)=>{
+        const result = await bookingsCollection.find().toArray()
+        res.send(result)
+    })
 
 
 
