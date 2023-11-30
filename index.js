303d7356c5bd72b4540476efe20df8c15850153f
Mohammadhridoy <mdridoykha019@gmail.com>
2023-11-30 21:46:58 +0600

    added payment system

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000; 
 
 // middware
@@ -31,6 +32,7 @@ async function run() {
 
     const usrtypeCollection = client.db('shipsy').collection('user');
     const bookingsCollection = client.db('shipsy').collection('bookings')
+    const reviewsCollection = client.db('shipsy').collection('reviews')
 
     // store user info in database
     app.post('/user', async(req, res)=>{
@@ -76,6 +78,67 @@ async function run() {
         const result = await usrtypeCollection.find(query).toArray()
         res.send(result)
     })
+    // get all deliever man list from user collection
+    app.get('/user/deliverman',async(req, res)=>{
+        const query = {role: 'deliveryman'}
+        const result = await usrtypeCollection.find(query).toArray()
+        res.send(result)
+    })
+    // get deliver info 
+    app.get('/user/delivery/:email', async(req, res)=>{
+        const email = req.params.email
+        
+        const query = {email: email}
+        const user = await usrtypeCollection.findOne(query)
+        let delivery = false;
+        
+        if(user){
+            delivery = user?.role === 'deliveryman'
+        }
+        
+        res.send({delivery})
+        
+    })
+    // make a deliveryman
+    app.patch('/user/delivery/:id', async(req, res)=>{
+        const id = req.params.id
+        const filter = {_id: new ObjectId(id)}
+        const updateInfo = {
+            $set:{
+                role:'deliveryman'
+            }
+        }
+        const result = await usrtypeCollection.updateOne(filter, updateInfo)
+        res.send(result)
+    })
+    // get admin info 
+    app.get('/user/admin/:email', async(req, res)=>{
+        const email = req.params.email;
+
+        const query = {email: email}
+        const user = await usrtypeCollection.findOne(query)
+        let admin = false;
+        if(user){
+            admin = user?.role === 'admin'
+        }
+        res.send({admin})
+    })
+// make admin 
+    app.patch('/user/admin/:id', async(req, res)=>{
+        const id = req.params.id 
+        const fillter = {_id: new ObjectId(id)}
+        const updateDoc ={
+            $set:{
+                role: 'admin'
+            }
+        }
+        const result = await usrtypeCollection.updateOne(fillter, updateDoc)
+        res.send(result)
+    })
+
+
+
+    // start bookings apis 
 
     // parcels post in database 
     app.post('/bookings', async(req, res)=>{
@@ -95,6 +158,7 @@ async function run() {
 
         const result = await bookingsCollection.find(query).toArray()
         res.send(result)
+        
     })
 
     app.get("/bookings/:id", async(req, res ) =>{
@@ -126,6 +190,24 @@ async function run() {
         const result = await bookingsCollection.updateOne(filter, updateInfo, options)
         res.send(result)
       })
+    //   admin update bookings info 
+    app.put('/bookings/admin/update/:id', async(req, res)=>{
+        const id = req.params.id
+        const filter ={_id: new ObjectId(id ) }
+        const options = {upsert: true}
+        const updatedInfo = req.body;
+        const updateDoc = {
+            $set:{
+                status: "On The Way",
+                approximateDeliveryDate: updatedInfo.approximateDeliveryDate,
+                deliverymanid: updatedInfo.deliverymanid,
+                deliverymanemail: updatedInfo.deliverymanemail
+            }
+        }
+        
+        const result = await bookingsCollection.updateOne(filter, updateDoc, options)
+        res.send(result)
+    })
     //   total bookigns fillter by email
     app.get('/bookings', async(req, res)=>{
         const email = req.query.email
@@ -141,6 +223,72 @@ async function run() {
         res.send(result)
     })
 
+    // update status from bookings info 
+    app.patch('/bookings/cancel/:id', async(req, res) =>{
+        const id = req.params.id
+        const filter = {_id: new ObjectId(id)}
+        const updateStatus ={
+            $set:{
+                status: 'cancel'
+            }
+        }
+        const result = await bookingsCollection.updateOne(filter, updateStatus)
+        res.send(result)
+    })
+    // update status from bookings info 
+    app.patch('/bookings/delivery/:id', async(req, res)=>{
+        const id = req.params.id
+        const filter= {_id: new ObjectId(id)}
+        const updateStatus ={
+            $set:{
+                status:'delivered'
+            }
+        }
+        const result = await bookingsCollection.updateOne(filter, updateStatus)
+        res.send(result)
+    })
+
+    // get bookings information by filter delivery man id 
+    app.get('/bookings/deliverinfo/:email', async(req, res)=>{
+        const email = req.params.email 
+        const query = {deliverymanemail:email}
+
+        const result = await bookingsCollection.find(query).toArray()
+       
+        res.send(result) 
+    })
+//   post reviews in reviewsCollection 
+   app.post('/reviews', async(req, res)=>{
+        const review = req.body;
+
+        const result = await reviewsCollection.insertOne(review)
+        res.send(result)
+   })
+//    get reviews info base on email 
+   app.get('/reviews/:email', async(req, res)=>{
+        const email = req.params.email
+        const filter = {delliveryManEmail: email}
+        
+        const result = await reviewsCollection.find(filter).toArray()
+        res.send(result)
+   })
+// Payment information
+app.post('/create-payment-intent', async(req, res)=>{
+    const { price } = req.body;
+    const amonut = parseInt(price*100)
+    // console.log(price)
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: amonut,  
+        currency: "usd",
+        payment_method_types: ['card']
+
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+
+})
 
 
     // Send a ping to confirm a successful connection
